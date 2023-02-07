9be25a494f050b866835f638fca26e5444efd700
unknown <swu341457@gmail.com>
2023-02-07 21:23:24 +0100

    commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/ntrvagwedwxa'rofpfeafe tgrwg   五邵阳p.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='div1'>efeageasgae</div>
   );
 }
 
